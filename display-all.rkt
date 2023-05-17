19d921e51283735d8ce6f780d0a386a064a29226
Sundström Valter <mail@valter.nu>
2023-05-17 11:27:30 +0200

    ...

--- display-all.rkt
+++ display-all.rkt
@@ -1,6 +1,14 @@
 #lang racket
 (require syntax/wrap-modbeg)
 
+(define (item-to-string val)
+  (cond
+    [(procedure? val) (item-to-string (val))]
+    [(string? val) val]
+    [(number? val) (number->string val)]
+    [(symbol? val) (symbol->string val)]))
+  
+
 ; https://stackoverflow.com/questions/45786187/how-to-maintain-define-functionality-with-custom-module-begin
 
 (provide
@@ -10,4 +18,8 @@
 (define-syntax module-begin (make-wrapping-module-begin #'wrap-expression))
 (define-syntax (wrap-expression stx)
   (syntax-case stx ()
-    [(_ expr) #'(printf "~a~n" expr)]))
+    [(_ expr) 
+     #'(printf "~a~n" 
+         (if (list? expr)
+             (string-join (map item-to-string (flatten expr)) "")
+             (item-to-string expr)))]))
