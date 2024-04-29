5e2743ae5d774d9ec9a1b027974281cc76babe2f
Sundström Valter <mail@valter.nu>
2024-04-30 01:17:54 +0200

    Remove figref and tabref

--- simple_commands.rkt
+++ simple_commands.rkt
@@ -35,12 +35,7 @@
 (define (label name) (command "label" name))
 (define (eqref name) (command "eqref" name))
 (define (ref name) (command "ref" name))
-(define (figref name) (list
-                        "Figure "
-                        (ref name)))
-(define (tabref name) (list
-                        "Table "
-                        (ref name)))
+
 (define cdot (command 'cdot))
 (define Delta (command "Delta"))
 (define delta (command "delta"))
